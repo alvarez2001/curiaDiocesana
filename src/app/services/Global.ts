6a3cdef6cdfe5f89917bc0e59c0a6eaa44a77191
Jose Miguel <66285893+alvarez2001@users.noreply.github.com>
2020-10-18 18:57:33 -0400

    Update Global.ts

--- src/app/services/Global.ts
+++ src/app/services/Global.ts
@@ -1,5 +1,3 @@
 export var Global = {
-  aaa:'http://192.168.1.3/sgp/public/api/',
-  url:'https://backendsgp.diocesisdecarupano.org/api/'
-  //url:'http://diocesisdecarupano.org/sgp/public/api/'
+  url:'http://192.168.1.3/sgp/public/api/',
 }
